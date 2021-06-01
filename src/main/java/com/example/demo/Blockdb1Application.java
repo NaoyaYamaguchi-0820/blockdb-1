64f7acc66ffd25d4184121040369929151daf8e4
NaoyaYamaguchi <55648796+NaoyaYamaguchi-0820@users.noreply.github.com>
2021-06-01 19:21:17 +0900

    first commit

---
+++ src/main/java/com/example/demo/Blockdb1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blockdb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blockdb1Application.class, args);
+	}
+
+}
